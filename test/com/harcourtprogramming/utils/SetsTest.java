9cdf2a1b3957f85f048b9dc7be69ed19e3c1df45
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2011-06-06 18:43:50 +0100

    Added SetTest

---
+++ test/com/harcourtprogramming/utils/SetsTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.harcourtprogramming.utils;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Benedict
+ */
+public class SetsTest
+{
+	
+	public SetsTest()
+	{
+	}
+	
+}
