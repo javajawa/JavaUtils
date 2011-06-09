b5435a081aae524de998b45770f66a88b441842f
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2011-06-09 10:32:45 +0100

    Formatting

--- src/com/harcourtprogramming/utils/FileUtils.java
+++ src/com/harcourtprogramming/utils/FileUtils.java
@@ -8,71 +8,69 @@ import java.io.File;
  */
 public final class FileUtils
 {
-
-  public static String realtivePath(final File base, final File target)
-  {
+	public static String realtivePath(final File base, final File target)
+	{
 		final File baseDir;
-    if (!base.isDirectory())
-    {
-      baseDir = base.getParentFile();
-    }
+		if (!base.isDirectory())
+		{
+			baseDir = base.getParentFile();
+		}
 		else
 		{
 			baseDir = base;
 		}
 
-    final String[] fromPath = explodedPath(baseDir);
-    final String[] toPath = explodedPath(target);
+		final String[] fromPath = explodedPath(baseDir);
+		final String[] toPath = explodedPath(target);
 
-    final int common;
-    int i;
+		final int common;
+		int i;
 
-    for (i = 0; i < Math.min(fromPath.length, toPath.length); i++)
-    {
-      if (!fromPath[i].equals(toPath[i]))
-      {
-        break;
-      }
-    }
+		for (i = 0; i < Math.min(fromPath.length, toPath.length); i++)
+		{
+			if (!fromPath[i].equals(toPath[i]))
+			{
+				break;
+			}
+		}
 
-    common = i;
+		common = i;
 
-    if (i == 0)
-    {
-      return target.getPath();
-    }
+		if (i == 0)
+		{
+			return target.getPath();
+		}
 
-    StringBuilder ret = new StringBuilder();
+		StringBuilder ret = new StringBuilder();
 
-    for (i = common; i < fromPath.length; i++)
-    {
-      ret.append("..");
-      ret.append(File.separatorChar);
-    }
+		for (i = common; i < fromPath.length; i++)
+		{
+			ret.append("..");
+			ret.append(File.separatorChar);
+		}
 
-    for (i = common; i < toPath.length; i++)
-    {
-      ret.append(toPath[i]);
-      ret.append(File.separatorChar);
-    }
+		for (i = common; i < toPath.length; i++)
+		{
+			ret.append(toPath[i]);
+			ret.append(File.separatorChar);
+		}
 
-    ret.deleteCharAt(ret.length() - 1);
-    return ret.toString();
+		ret.deleteCharAt(ret.length() - 1);
+		return ret.toString();
 
-  }
+	}
 
-  public static String[] explodedPath(File f)
-  {
-    if (File.separatorChar == '\\')
-    {
-      return f.getPath().split("\\\\"); // Because I hate you java and you lake of split on char or string.
-    }
-    return f.getPath().split(File.separator);
-  }
+	public static String[] explodedPath(File f)
+	{
+		if (File.separatorChar == '\\')
+		{
+			return f.getPath().split("\\\\"); // Because I hate you java and you lake of split on char or string.
+		}
+		return f.getPath().split(File.separator);
+	}
 
-  private FileUtils()
-  {
-    // Nothing to see here. Move along, citizen.
-  }
-
+	private FileUtils()
+	{
+		// Nothing to see here. Move along, citizen.
+	}
 }
--- src/com/harcourtprogramming/utils/Sets.java
+++ src/com/harcourtprogramming/utils/Sets.java
@@ -7,25 +7,26 @@ import java.util.Set;
  *
  * @author Benedict
  */
-public final class Sets {
-  private Sets()
-  {
-    // Nothing to see here. Move along, citizen!
-  }
+public final class Sets
+{
+	private Sets()
+	{
+		// Nothing to see here. Move along, citizen!
+	}
 
-  public static <T> Set<T> union(Set<? extends T> a, Set<? extends T> b)
-  {
-    Set<T> u = new HashSet<T>(a);
-    u.addAll(b);
+	public static <T> Set<T> union(Set<? extends T> a, Set<? extends T> b)
+	{
+		Set<T> u = new HashSet<T>(a);
+		u.addAll(b);
 
-    return u;
-  }
+		return u;
+	}
 
-  public static <T> Set<T> intersection(Set<? extends T> a, Set<? extends T> b)
-  {
-    Set<T> u = new HashSet<T>(a);
-    u.retainAll(b);
+	public static <T> Set<T> intersection(Set<? extends T> a, Set<? extends T> b)
+	{
+		Set<T> u = new HashSet<T>(a);
+		u.retainAll(b);
 
-    return u;
-  }
+		return u;
+	}
 }
