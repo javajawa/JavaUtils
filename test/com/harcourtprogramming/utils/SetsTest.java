4541eca4f62a75bd65cff27a437aa5aa05026cb7
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2011-07-07 09:03:27 +0100

    Wrote JUnit tests for Sets

--- test/com/harcourtprogramming/utils/SetsTest.java
+++ test/com/harcourtprogramming/utils/SetsTest.java
@@ -1,21 +1,60 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.harcourtprogramming.utils;
 
+import java.util.Arrays;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.TreeSet;
+import static junit.framework.Assert.assertEquals;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
- *
- * @author Benedict
+ * TODO: Document SetsTest
+ * @author Benedict Harcourt
  */
 public class SetsTest
 {
-	
 	public SetsTest()
 	{
 	}
-	
+
+	/**
+	 * TODO: Document unionTest()
+	 */
+	@Test
+	public void unionTest()
+	{
+		Set<Integer> a = new HashSet<Integer>(Arrays.asList(1, 2, 3));
+		Set<Integer> b = new TreeSet<Integer>(Arrays.asList(2, 3, 4));
+
+		Set<Integer> c = Sets.union(a, b);
+		assertEquals(4, c.size());
+
+		c.remove(3);
+		assertEquals(3, c.size());
+		c.remove(1);
+		assertEquals(2, c.size());
+		c.remove(2);
+		assertEquals(1, c.size());
+		c.remove(4);
+		assertEquals(0, c.size());
+
+	}
+
+	/**
+	 * TODO: Document intersectionTest()
+	 */
+	@Test
+	public void intersectionTest()
+	{
+		Set<Integer> a = new HashSet<Integer>(Arrays.asList(1, 2, 3));
+		Set<Integer> b = new TreeSet<Integer>(Arrays.asList(2, 3, 4));
+
+		Set<Integer> c = Sets.intersection(a, b);
+		assertEquals(2, c.size());
+
+		c.remove(3);
+		assertEquals(1, c.size());
+		c.remove(2);
+		assertEquals(0, c.size());
+	}
 }
