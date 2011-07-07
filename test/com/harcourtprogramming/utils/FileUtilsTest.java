037865ae9c48ed80ce0459966fb6570f3347be78
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2011-07-07 09:01:37 +0100

    Formatting etc FileUtilsTest
     - Spaces -> Tabs
     - Placeholder TODOs for JavaDoc
     - Removed system.out calls

--- test/com/harcourtprogramming/utils/FileUtilsTest.java
+++ test/com/harcourtprogramming/utils/FileUtilsTest.java
@@ -1,6 +1,5 @@
 package com.harcourtprogramming.utils;
 
-import com.harcourtprogramming.utils.FileUtils;
 import java.io.File;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -11,97 +10,92 @@ import static org.junit.Assert.*;
  */
 public class FileUtilsTest
 {
+	public FileUtilsTest()
+	{
+	}
 
-  public FileUtilsTest()
-  {
-  }
+	/**
+	 * Test of realtivePath method, of class FileUtils.
+	 * TODO: Document testRealtivePathUnix()
+	 */
+	@Test
+	public void testRealtivePathUnix()
+	{
+		File base = new File("/etc/init.d/apache");
+		File target = new File("/etc/passwd");
+		String expResult = ".." + File.separatorChar + "passwd";
 
-  /**
-   * Test of realtivePath method, of class FileUtils.
-   */
-  @Test
-  public void testRealtivePathUnix()
-  {
-    System.out.println("Realtive Path (UNIX)");
+		String result = FileUtils.realtivePath(base, target);
 
-    File base = new File("/etc/init.d/apache");
-    File target = new File("/etc/passwd");
-    String expResult = ".." + File.separatorChar + "passwd";
+		assertEquals(expResult, result);
+	}
 
-    String result = FileUtils.realtivePath(base, target);
+	/**
+	 * Test of realtivePath method, of class FileUtils.
+	 * TODO: Document testRealtivePathWindows1()
+	 */
+	@Test
+	public void testRealtivePathWindows1()
+	{
+		File base = new File("C:\\Program Files\\Test\\text.exe");
+		File target = new File("C:\\Windows\\System32\\virius.exe");
 
-    assertEquals(expResult, result);
-  }
+		String expResult = "..\\..\\Windows\\System32\\virius.exe";
 
-  /**
-   * Test of realtivePath method, of class FileUtils.
-   */
-  @Test
-  public void testRealtivePathWindows1()
-  {
-    System.out.println("Realtive Path (Windows)");
+		String result = FileUtils.realtivePath(base, target);
 
-    File base = new File("C:\\Program Files\\Test\\text.exe");
-    File target = new File("C:\\Windows\\System32\\virius.exe");
+		assertEquals(expResult, result);
+	}
 
-    String expResult = "..\\..\\Windows\\System32\\virius.exe";
+	/**
+	 * Test of realtivePath method, of class FileUtils.
+	 * TODO: Document testRealtivePathWindows2()
+	 */
+	@Test
+	public void testRealtivePathWindows2()
+	{
+		File base = new File("C:\\Windows\\System32\\virius.exe");
+		File target = new File("D:\\users\\admin\\loadvirius.dll");
+		String expResult = "D:\\users\\admin\\loadvirius.dll";
 
-    String result = FileUtils.realtivePath(base, target);
+		String result = FileUtils.realtivePath(base, target);
 
-    assertEquals(expResult, result);
-  }
+		assertEquals(expResult, result);
+	}
 
-  /**
-   * Test of realtivePath method, of class FileUtils.
-   */
-  @Test
-  public void testRealtivePathWindows2()
-  {
-    System.out.println("Realtive Path (Windows) - 2");
+	/**
+	 * Test of explodedPath method, of class FileUtils.
+	 * TODO: Document testExplodedPathUnix()
+	 */
+	@Test
+	public void testExplodedPathUnix()
+	{
+		File f = new File("/etc/passwd");
+		String[] expResult = new String[3];
 
-    File base = new File("C:\\Windows\\System32\\virius.exe");
-    File target = new File("D:\\users\\admin\\loadvirius.dll");
-    String expResult = "D:\\users\\admin\\loadvirius.dll";
+		expResult[0] = "";
+		expResult[1] = "etc";
+		expResult[2] = "passwd";
 
-    String result = FileUtils.realtivePath(base, target);
+		String[] result = FileUtils.explodedPath(f);
+		assertArrayEquals(expResult, result);
+	}
 
-    assertEquals(expResult, result);
-  }
+	/**
+	 * Test of explodedPath method, of class FileUtils.
+	 * TODO: Document testExplodedPathWindows()
+	 */
+	@Test
+	public void testExplodedPathWindows()
+	{
+		File f = new File("C:\\Windows\\system32");
+		String[] expResult = new String[3];
 
-  /**
-   * Test of explodedPath method, of class FileUtils.
-   */
-  @Test
-  public void testExplodedPathUnix()
-  {
-    System.out.println("explodedPath");
-    File f = new File("/etc/passwd");
-    String[] expResult = new String[3];
+		expResult[0] = "C:";
+		expResult[1] = "Windows";
+		expResult[2] = "system32";
 
-    expResult[0] = "";
-    expResult[1] = "etc";
-    expResult[2] = "passwd";
-
-    String[] result = FileUtils.explodedPath(f);
-    assertArrayEquals(expResult, result);
-  }
-
-  /**
-   * Test of explodedPath method, of class FileUtils.
-   */
-  @Test
-  public void testExplodedPathWindows()
-  {
-    System.out.println("explodedPath");
-    File f = new File("C:\\Windows\\system32");
-    String[] expResult = new String[3];
-
-    expResult[0] = "C:";
-    expResult[1] = "Windows";
-    expResult[2] = "system32";
-
-    String[] result = FileUtils.explodedPath(f);
-    assertArrayEquals(expResult, result);
-  }
-
+		String[] result = FileUtils.explodedPath(f);
+		assertArrayEquals(expResult, result);
+	}
 }
